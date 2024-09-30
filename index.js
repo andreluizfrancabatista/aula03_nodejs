50e06a5175b9461dc997e0832a98da331f926dfe
ANDRE LUIZ FRANCA BATISTA <01411769627@pedag.local>
2024-09-30 15:12:39 -0300

    First commit

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require('express')
-
+const mongoose = require('mongoose')
 require('dotenv').config()
+
+const mongoString = process.env.DATABASE_URL
+
+mongoose.connect(mongoString)
+
+const database = mongoose.connection
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('DB connectado')
+})
+
+//Criar a applicação
+//Criar as rotas
+//Criar a lógica do negócio
 
 const port = 3000
 const app = express()
@@ -13,5 +31,5 @@ const senha = process.env.SENHA
 const usuario = process.env.USUARIO
 
 app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port} com usuário ${usuario} e senha ${senha}`)
+    console.log(`Servidor rodando na porta ${port}`)
 })
